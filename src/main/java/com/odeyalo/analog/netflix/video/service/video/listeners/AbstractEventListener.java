483dbca15f904294e6197b832296a8f5f66e8c27
Odeyalo <theprogrammerinfuture@gmail.com>
2022-07-24 19:32:10 +0300

    Contains logger and will be added a logic

---
+++ src/main/java/com/odeyalo/analog/netflix/video/service/video/listeners/AbstractEventListener.java
@@ -0,0 +1,9 @@
+package com.odeyalo.analog.netflix.video.service.video.listeners;
+
+import com.odeyalo.analog.netflix.video.service.video.events.Event;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class AbstractEventListener<T extends Event> implements EventListener<T> {
+    protected final Logger logger = LoggerFactory.getLogger(AbstractEventListener.class);
+}
