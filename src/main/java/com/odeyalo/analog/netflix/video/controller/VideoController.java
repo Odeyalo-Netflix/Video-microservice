7ed476632229c3f654330a4ee3d6dc6412a83e51
Odeyalo <theprogrammerinfuture@gmail.com>
2022-07-29 19:28:28 +0300

    Removed streamVideo method, changed response for '/upload' endpoint

--- src/main/java/com/odeyalo/analog/netflix/video/controller/VideoController.java
+++ src/main/java/com/odeyalo/analog/netflix/video/controller/VideoController.java
@@ -10,26 +10,26 @@ import com.odeyalo.analog.netflix.video.service.facade.VideoUploadServiceFacade;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.support.ResourceRegion;
-import org.springframework.http.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/video")
+@CrossOrigin("*")
 public class VideoController {
     private final VideoManager manager;
     private final Logger logger = LoggerFactory.getLogger(VideoController.class);
     private final VideoUploadServiceFacade videoUploadServiceFacade;
-    private final RestTemplate restTemplate;
+
     @Autowired
-    public VideoController(VideoManager manager, VideoUploadServiceFacade videoUploadServiceFacade, RestTemplate restTemplate) {
+    public VideoController(VideoManager manager, VideoUploadServiceFacade videoUploadServiceFacade) {
         this.manager = manager;
         this.videoUploadServiceFacade = videoUploadServiceFacade;
-        this.restTemplate = restTemplate;
     }
 
     @GetMapping("/{id}")
@@ -39,19 +39,15 @@ public class VideoController {
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    @GetMapping("/watch")
-    public ResponseEntity<?> streamVideo(@RequestParam(name = "v") String id, @RequestHeader(name = "range", required = false) String range) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setRange(Collections.singletonList(HttpRange.createByteRange(Long.parseLong(range))));
-        HttpEntity<String> entity = new HttpEntity<>("body", headers);
-        return this.restTemplate.exchange("http://localhost:8084/api/v1/video/watch?videoId=" + id, HttpMethod.GET, entity, ResourceRegion.class);
-    }
+
     @PostMapping(value = "/upload")
     public ResponseEntity<?> uploadVideo(@RequestPart UploadVideoDTO dto, @RequestPart MultipartFile video, @RequestPart MultipartFile poster) throws VideoUploadException, PosterUploadException {
         this.logger.info("Dto: {}, video: {}, poster: {}", dto, video.getOriginalFilename(), poster.getOriginalFilename());
 
         this.videoUploadServiceFacade.uploadVideo(dto, video, poster);
-        return ResponseEntity.ok().build();
+        Map<String, Object> body = new HashMap<>();
+        body.put("status", "VIDEO_UPLOADING_STARTED");
+        return ResponseEntity.ok(body);
     }
 
 
