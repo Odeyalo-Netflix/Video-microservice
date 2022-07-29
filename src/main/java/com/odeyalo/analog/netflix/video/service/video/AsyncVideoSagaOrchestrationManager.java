2d3c85f8827be0bcd1710a0e577d537037064992
Odeyalo <theprogrammerinfuture@gmail.com>
2022-07-29 19:22:05 +0300

    Orchestration manager that process saga in async mode

--- src/main/java/com/odeyalo/analog/netflix/video/service/video/AsyncVideoSagaOrchestrationManager.java
+++ src/main/java/com/odeyalo/analog/netflix/video/service/video/AsyncVideoSagaOrchestrationManager.java
@@ -1,15 +1,19 @@
 package com.odeyalo.analog.netflix.video.service.video;
 
+import com.odeyalo.analog.netflix.video.dto.UploadVideoData;
 import com.odeyalo.analog.netflix.video.entity.Video;
+import com.odeyalo.analog.netflix.video.exceptions.WorkflowStepException;
+import com.odeyalo.analog.netflix.video.service.utils.MultipartFileUtils;
 import com.odeyalo.analog.netflix.video.service.video.dto.UploadVideoInformation;
 import com.odeyalo.analog.netflix.video.service.video.steps.VideoSaveWorkflowStep;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.PostConstruct;
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -17,7 +21,6 @@ public class AsyncVideoSagaOrchestrationManager implements SagaOrchestrationMana
 
     private final List<VideoSaveWorkflowStep> steps;
     private final Logger logger = LoggerFactory.getLogger(AsyncVideoSagaOrchestrationManager.class);
-
     @Autowired
     public AsyncVideoSagaOrchestrationManager(List<VideoSaveWorkflowStep> steps) {
         this.steps = steps;
@@ -31,8 +34,21 @@ public class AsyncVideoSagaOrchestrationManager implements SagaOrchestrationMana
     @Override
     public void processSagaTransaction(UploadVideoInformation information) {
         Video video = Video.builder().build();
-        this.logger.info("Starting video saga process with information: {}", information);
-        this.steps.forEach(step -> step.process(information, video));
-        this.logger.info("Saga successful finished");
+        try {
+            MultipartFile videoFile = MultipartFileUtils.copyMultipartFile(information.getVideoFile());
+            MultipartFile posterFile = MultipartFileUtils.copyMultipartFile(information.getPosterFile());
+            UploadVideoData data = information.getData();
+            UploadVideoInformation copiedInfo = new UploadVideoInformation(data, videoFile, posterFile);
+            Thread thread = new Thread(() -> {
+                for (VideoSaveWorkflowStep step : steps) {
+                    step.process(copiedInfo, video);
+                }
+            });
+            thread.setName("VIDEO_UPLOADER_THREAD_1");
+            thread.start();
+        } catch (IOException ex) {
+            this.logger.error("Error during file copying.", ex);
+            throw new WorkflowStepException("Cannot process video to server!");
+        }
     }
 }
