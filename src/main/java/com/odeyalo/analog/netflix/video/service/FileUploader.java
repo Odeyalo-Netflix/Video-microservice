f2565ad4f502d33f5bdf428183d24627ba922aba
Odeyalo <theprogrammerinfuture@gmail.com>
2022-07-29 19:22:59 +0300

    Upload file

--- src/main/java/com/odeyalo/analog/netflix/video/service/FileUploader.java
+++ src/main/java/com/odeyalo/analog/netflix/video/service/FileUploader.java
@@ -3,6 +3,10 @@ package com.odeyalo.analog.netflix.video.service;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface FileUploader<R> {
-
+    /**
+     *
+     * @param file - file to upload
+     * @return - return generic type
+     */
     R uploadFile(MultipartFile file);
 }
