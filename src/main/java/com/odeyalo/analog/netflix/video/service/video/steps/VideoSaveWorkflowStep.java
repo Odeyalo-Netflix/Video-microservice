8087ed93ad5788bac6f1df1c258243661a29b4b0
Odeyalo <theprogrammerinfuture@gmail.com>
2022-07-29 19:25:11 +0300

    Workflow step to video saving

--- src/main/java/com/odeyalo/analog/netflix/video/service/video/steps/VideoSaveWorkflowStep.java
+++ src/main/java/com/odeyalo/analog/netflix/video/service/video/steps/VideoSaveWorkflowStep.java
@@ -1,6 +1,7 @@
 package com.odeyalo.analog.netflix.video.service.video.steps;
 
 import com.odeyalo.analog.netflix.video.entity.Video;
+import com.odeyalo.analog.netflix.video.exceptions.WorkflowStepException;
 import com.odeyalo.analog.netflix.video.service.video.dto.UploadVideoInformation;
 
 public interface VideoSaveWorkflowStep {
@@ -9,6 +10,6 @@ public interface VideoSaveWorkflowStep {
      * @param information - to get information about video
      * @param rawVideo - to build video entity
      */
-    void process(UploadVideoInformation information, Video rawVideo);
+    void process(UploadVideoInformation information, Video rawVideo) throws WorkflowStepException;
 
 }
