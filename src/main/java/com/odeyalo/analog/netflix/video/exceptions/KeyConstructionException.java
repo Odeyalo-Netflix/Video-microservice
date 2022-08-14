560dd5b5021dbb858978247267555ace9f026444
Odeyalo <theprogrammerinfuture@gmail.com>
2022-08-14 12:35:35 +0300

    Exception that can be occurred when cannot create a key from file or other resource

---
+++ src/main/java/com/odeyalo/analog/netflix/video/exceptions/KeyConstructionException.java
@@ -0,0 +1,12 @@
+package com.odeyalo.analog.netflix.video.exceptions;
+
+public class KeyConstructionException extends Exception {
+
+    public KeyConstructionException(String message) {
+        super(message);
+    }
+
+    public KeyConstructionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
