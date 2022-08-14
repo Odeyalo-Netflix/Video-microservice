4707c154d8847a7ec09cd52368846c6c2d7f2970
Odeyalo <theprogrammerinfuture@gmail.com>
2022-08-14 12:32:51 +0300

    A simple configuration that creates beans and auto registry PublicKey in RsaTokenHolder

---
+++ src/main/java/com/odeyalo/analog/netflix/video/configuration/JwtTokenRsaKeyConfiguration.java
@@ -0,0 +1,36 @@
+package com.odeyalo.analog.netflix.video.configuration;
+
+import com.odeyalo.analog.netflix.video.exceptions.KeyConstructionException;
+import com.odeyalo.analog.netflix.video.support.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.security.PublicKey;
+
+@Configuration
+public class JwtTokenRsaKeyConfiguration {
+    private final Logger logger = LoggerFactory.getLogger(JwtTokenRsaKeyConfiguration.class);
+
+    @Bean
+    public FileReader fileReader() {
+        return new SimpleFileReader();
+    }
+
+    @Bean
+    public RsaPublicKeyResolver rsaPublicKeyResolver(
+            @Value("${app.security.rsa.keys.public.filename}") String fileName,
+            FileReader fileReader) {
+        return new FileRsaPublicKeyResolver(fileName, fileReader);
+    }
+
+    @Bean
+    Void registryPublicKey(RsaPublicKeyResolver rsaPublicKeyResolver) throws KeyConstructionException {
+        PublicKey publicKey = rsaPublicKeyResolver.getPublicKey();
+        this.logger.info("Public key was set with size: {}", publicKey.getEncoded().length);
+        RsaTokenHolder.setPublicKey(publicKey);
+        return null;
+    }
+}
