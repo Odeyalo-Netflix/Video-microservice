4d86fb3b72a8c35a8deea25fb615759d24266556
Odeyalo <theprogrammerinfuture@gmail.com>
2022-08-23 21:39:32 +0300

    Added findAllByUserId

--- src/main/java/com/odeyalo/analog/netflix/video/repository/VideoRepository.java
+++ src/main/java/com/odeyalo/analog/netflix/video/repository/VideoRepository.java
@@ -4,7 +4,10 @@ import com.odeyalo.analog.netflix.video.entity.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VideoRepository extends JpaRepository<Video, String> {
 
+    List<Video> findAllByUserId(String userId);
 }
