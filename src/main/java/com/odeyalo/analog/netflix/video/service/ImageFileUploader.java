dcf22c4a68531534898bcfa1149ac9537cce8729
Odeyalo <theprogrammerinfuture@gmail.com>
2022-10-07 14:08:05 +0300

    Now returns ImageSuccessfulUploadedResponseDTO

--- src/main/java/com/odeyalo/analog/netflix/video/service/ImageFileUploader.java
+++ src/main/java/com/odeyalo/analog/netflix/video/service/ImageFileUploader.java
@@ -1,6 +1,7 @@
 package com.odeyalo.analog.netflix.video.service;
 
 import com.odeyalo.support.clients.filestorage.ImageControllerClient;
+import com.odeyalo.support.clients.filestorage.dto.ImageSuccessfulUploadedResponseDTO;
 import com.odeyalo.support.clients.filestorage.dto.SuccessUploadImageResponseDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,24 +17,23 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Objects;
-import java.util.concurrent.CompletableFuture;
 
 @Service
-public class ImageFileUploader implements FileUploader<SuccessUploadImageResponseDTO> {
+public class ImageFileUploader implements FileUploader<ImageSuccessfulUploadedResponseDTO> {
     private final ImageControllerClient client;
     private final Logger logger = LoggerFactory.getLogger(ImageFileUploader.class);
+
     @Autowired
-    RestTemplate restTemplate;
     public ImageFileUploader(ImageControllerClient client) {
         this.client = client;
     }
 
 
     @Override
-    public SuccessUploadImageResponseDTO uploadFile(MultipartFile file) {
-        ResponseEntity<SuccessUploadImageResponseDTO> response = this.client.saveImage(file);
-        SuccessUploadImageResponseDTO body = response.getBody();
-        this.logger.info("Success uploaded image. Image id: {}.", Objects.requireNonNull(body).getId());
+    public ImageSuccessfulUploadedResponseDTO uploadFile(MultipartFile file) {
+        ResponseEntity<ImageSuccessfulUploadedResponseDTO> response = this.client.saveImage(file);
+        ImageSuccessfulUploadedResponseDTO body = response.getBody();
+        this.logger.info("Success uploaded image. Image id: {}.", Objects.requireNonNull(body).getImageId());
         return body;
     }
 
